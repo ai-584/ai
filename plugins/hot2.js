19edfaeb520191d252905a4caa610c9380286df2
ai-584 <mi9991680@gmail.com>
2026-06-19 10:34:45 +0500

    ねふ

--- plugins/hot2.js
+++ plugins/hot2.js
@@ -42,7 +42,7 @@ function getPinterestSearch(query) {
 
 cmd({
     pattern: "smexy",
-    alias: ["", ""],
+    alias: ["smexy", "smexy"],
     react: "📌",
     desc: "Search images on hot girl",
     category: "search",
