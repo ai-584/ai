3ee8156be043100703970ab39cdd2ae04f3a5952
ai-584 <mi9991680@gmail.com>
2026-06-19 10:39:12 +0500

    なはたさたさたさはち

--- plugins/hotcouple.js
+++ plugins/hotcouple.js
@@ -42,7 +42,7 @@ function getPinterestSearch(query) {
 
 cmd({
     pattern: "hotcouple",
-    alias: ["", ""],
+    alias: ["hotcouple", "hotcouple"],
     react: "📌",
     desc: "Search images on hot girl",
     category: "search",
